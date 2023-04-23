b0fd64e893b547409ca82a692598c624bf1b2fde
Tobi511 <tobias.wegner@hsbi.de>
2023-04-23 20:05:25 +0200

    frostbolt cd test branch

--- game/src/ecs/entities/Hero.java
+++ game/src/ecs/entities/Hero.java
@@ -15,7 +15,7 @@ import graphic.Animation;
 public class Hero extends Entity {
 
     private final int fireballCoolDown = 5;
-    private final int frostBoltCoolDown = 1;
+    private final int frostBoltCoolDown = 4;
     private final float xSpeed = 0.3f;
     private final float ySpeed = 0.3f;
 
