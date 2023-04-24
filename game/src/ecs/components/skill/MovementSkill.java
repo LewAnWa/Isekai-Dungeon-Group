e0c4c3e2d8765c3452e95ed480e596d18f80ac5f
Tobi511 <tobias.wegner@hsbi.de>
2023-04-24 19:26:31 +0200

    dash funktioniert

--- game/src/ecs/components/skill/MovementSkill.java
+++ game/src/ecs/components/skill/MovementSkill.java
@@ -3,6 +3,9 @@ package ecs.components.skill;
 import ecs.components.VelocityComponent;
 import ecs.entities.Entity;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public abstract class MovementSkill implements ISkillFunction{
 
     private float heroSpeed;
@@ -21,5 +24,22 @@ public abstract class MovementSkill implements ISkillFunction{
                     ((VelocityComponent) hvc).setXVelocity(heroSpeed);
                     ((VelocityComponent) hvc).setYVelocity(heroSpeed);
                 });
+
+        Timer timer = new Timer();
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                entity.getComponent(VelocityComponent.class)
+                    .ifPresent(
+                        hvc -> {
+                            ((VelocityComponent) hvc).setXVelocity(0.3f);
+                            ((VelocityComponent) hvc).setYVelocity(0.3f);
+                        });
+                timer.cancel();
+            }
+        }, 80);
+
+
     }
 }
