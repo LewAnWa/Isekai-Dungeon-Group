c4cf974ad4178553f6d83788d96e11c550b962df
JustCallMeKK <kirill.kuhn@mx.de>
2023-06-04 15:59:23 +0200

    Added new idleAI type.

--- game/src/ecs/components/skill/DashSkill.java
+++ game/src/ecs/components/skill/DashSkill.java
@@ -4,9 +4,19 @@ public class DashSkill extends MovementSkill {
 
     /**
      * This Skill is a MovementSkill It can be used to Dash into the currently headed direction The
-     * Velocity the Hero will receive while dashing can be set here
+     * Velocity the Entity will receive while dashing can be set here
+     *
+     * @param entitySpeed the base speed of the entity.
+     */
+    public DashSkill(float entitySpeed) {
+        super(entitySpeed, 5);
+    }
+
+    /**
+     * This Skill is a MovementSkill It can be used to Dash into the currently headed direction The
+     * Velocity the Entity will receive while dashing can be set here
      */
     public DashSkill() {
-        super(1f, 5);
+        super(0.3f, 5);
     }
 }
