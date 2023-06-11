5dbff70367ff73e83da146317112579251da4a99
JustCallMeKK <kirill.kuhn@mx.de>
2023-06-11 02:45:08 +0200

    The LightSource (Entity) now has a LightSourceComponent.

--- game/src/ecs/entities/LightSource.java
+++ game/src/ecs/entities/LightSource.java
@@ -1,22 +1,21 @@
 package ecs.entities;
 
+import ecs.components.LightSourceComponent;
 import ecs.components.PositionComponent;
 import starter.Game;
 
 public class LightSource extends Entity {
 
-    private final float lightRadius;
-
     public LightSource(float lightRadius) {
-        this.lightRadius = lightRadius;
         setupPositionComponent();
+        setupLightSourceComponent(lightRadius);
+    }
+
+    private void setupLightSourceComponent(float lightRadius) {
+        new LightSourceComponent(this, lightRadius);
     }
 
     private void setupPositionComponent() {
         new PositionComponent(this, Game.currentLevel.getRandomFloorTile().getCoordinateAsPoint());
-    }
-
-    public float getLightRadius() {
-        return lightRadius;
     }
 }
